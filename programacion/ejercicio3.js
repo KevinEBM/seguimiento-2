9bd9b5c9750fd84cf43ce90b7464c5c21e8471ac
KevinEBM <kebuitrago_506@unihumboldt.edu.co>
2024-08-12 18:40:45 -0500

    Update ejercicio3.js
    mojojo

--- programacion/ejercicio3.js
+++ programacion/ejercicio3.js
@@ -3,27 +3,29 @@
     const lavadoraGrande = 4000
     const lavadoraPequeña = 3000
     const descuento = 0.03 
+let total
+
 let tipoLavadora = prompt("ingrese el tipo de lavadora 1 o 2") 
     
     // Calculamos el costo base dependiendo del tipo de lavadora
     let horasAquiler = prompt("ingrese horas de aquiler ")  ;
 
     if (tipoLavadora == 1) {
-        horasAquiler * lavadoraGrande
+       total = horasAquiler * lavadoraGrande
     } else if (tipoLavadora == 2) {
-        horasAquiler * lavadoraPequeña ;
+       total = horasAquiler * lavadoraPequeña ;
     } else {
         console.log("Tipo de lavadora no válido. Usa 1 para lavadora grande o 2 para lavadora pequeña.");
-        return;
+       
     }
 let cantidadLavadoras = prompt("ingrese cantidad de lavadoras a aquilar ")
 
     // Calculamos el costo total sin descuento
-    let costoTotal = horasAquiler * cantidadLavadoras 
+    let costoTotal = total * cantidadLavadoras 
     
     
     // Aplicamos el descuento si se alquilan más de 3 lavadoras
-    parseInt(prompt())
+   
     if (cantidadLavadoras > 3) {
         costoTotal -= costoTotal * descuento;
     }
